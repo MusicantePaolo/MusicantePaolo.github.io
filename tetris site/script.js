77fc5bb4265ad9fabb5bc4e40a631c635fd2b783
MusicantePaolo <145185364+MusicantePaolo@users.noreply.github.com>
2024-04-23 20:45:37 +0200

    *

--- tetris site/script.js
+++ tetris site/script.js
@@ -1,19 +1,19 @@
 window.onload = function() {
     var textElements = document.getElementsByClassName("text");
-    var colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#FFA500', '#FF4500', '#9400D3', '#FF1493']; // Aggiunto un altro colore
+    var colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#FFA500', '#FF4500', '#9400D3', '#FF1493']; //Colori tetris
 
     for (var j = 0; j < textElements.length; j++) {
         var text = textElements[j].innerText;
         var newText = "";
 
         for (var i = 0; i < text.length; i++) {
-            // Assegna un colore Tetris a ciascuna lettera
+            //Assegna un colore Tetris a ciascuna lettera
             var color = colors[i % colors.length];
-            // Crea un elemento span per ogni lettera con il colore Tetris
+            //Crea un elemento span per ogni lettera con il colore Tetris
             newText += '<span style="color:' + color + ';">' + text[i] + '</span>';
         }
 
-        // Aggiorna il contenuto dell'elemento span con le lettere colorate
+        //Aggiorna il contenuto dell'elemento span con le lettere colorate
         textElements[j].innerHTML = newText;
     }
 };
